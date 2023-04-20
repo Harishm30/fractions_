3190d79de25b09bfd015a6e31c53d307f586268a
shankara738 <119494040+shankara738@users.noreply.github.com>
2023-04-20 12:05:34 +0530

    Update fraction.js

--- fraction.js
+++ fraction.js
@@ -1,1 +1,12 @@
-console.log("hello")
+class add {
+  constructor(num1, num2) {
+    this.num1 = num1;
+    this.num2 = num2;
+  }
+  add() {
+    return this.num1 + this.num2;
+  }
+}
+
+let add = new add(21, 4);
+console.log(add.add()); 
