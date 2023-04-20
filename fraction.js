6a07be95da1577b2618f0a148a938a846864e57a
Harishm30 <117711949+Harishm30@users.noreply.github.com>
2023-04-20 12:12:14 +0530

    Update fraction.js

--- fraction.js
+++ fraction.js
@@ -1,1 +1,12 @@
-console.log("hello")
+class Multiply{
+  constructor(numerator, denominator) {
+    this.numerator = numerator;
+    this.denominator = denominator;
+  }
+  
+ multiply(other) {
+    const newNumerator = this.numerator * other.numerator;
+    const newDenominator = this.denominator * other.denominator;
+    return new Fraction(newNumerator, newDenominator);
+  }
+}
