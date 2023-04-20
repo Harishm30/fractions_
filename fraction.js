714dbd4154abc1182f15810c3ae83c503bd9f766
Rakshitha1005 <115693685+Rakshitha1005@users.noreply.github.com>
2023-04-20 12:03:26 +0530

    Update fraction.js

--- fraction.js
+++ fraction.js
@@ -1,1 +1,12 @@
-console.log("hello")
+class Divide {
+  constructor(num1, num2) {
+    this.num1 = num1;
+    this.num2 = num2;
+  }
+  divide() {
+    return this.num1 / this.num2;
+  }
+}
+
+let divide = new Divide(10, 5);
+console.log(divide.divide()); 
